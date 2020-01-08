85e8056071513bd03c6eaf2548787a988f046d02
Oksana Shadura <oksana.shadura@cern.ch>
2020-01-08 20:13:53 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ import os.path
 from setuptools import find_packages
 from setuptools import setup
 
+
+
 setup(name='coffea-bench',
       version='0.1',
-      description='Benchmarks for Coffea andf Laurelin',
+      description='Benchmarks for Coffea and Laurelin',
       author='Oksana Shadura',
       author_email='oksana.shadura@cern.ch',
-      url="https://github.com/oshadura/coffea-bench",
-      install_requires=["coffea", "pyspark", "numpy", "numba", "awkward"],
-      tests_require=["pytest","pytest-benchmark"],
-      setup_requires=["pytest-runner"],
-      packages='find_packages(exclude=["tests"])',
+      url='https://github.com/oshadura/coffea-bench',
+      install_requires=['coffea','pyspark','numpy','numba','awkward'],
+      tests_require=['pytest','pytest-benchmark','jinja2','pyarrow'],
+      setup_requires=['pytest-runner'],
+      test_suite = "tests",
 )
