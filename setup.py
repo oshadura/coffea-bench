82544901d9142e347493214e5cd01b4059442628
Ubuntu <ubuntu@coffea-bench.cern.ch>
2020-02-20 18:06:39 +0000

    Update pytest benchmark options

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='coffea-bench',
       author_email='oksana.shadura@cern.ch',
       url='https://github.com/oshadura/coffea-bench',
       install_requires=['coffea','pyspark','numpy','numba','awkward'],
-      tests_require=['pytest','pytest-benchmark','jinja2','pyarrow'],
+      tests_require=['pytest','pytest-benchmark','pytest-benchmark[histogram]','jinja2','pyarrow'],
       setup_requires=['pytest-runner'],
       test_suite = "tests",
 )
